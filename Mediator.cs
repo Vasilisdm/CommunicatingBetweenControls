9e02376e78612bd5c57566447345d8f5fd809de0
vasilisdimitriou <vasilisdmr@gmail.com>
2020-02-27 21:52:17 +0200

    Implemented the Mediator class and added a property which returns a singleton instance.

---
+++ Mediator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicatingBetweenControls
+{
+    public sealed class Mediator
+    {
+		private Mediator(){}
+
+        private static readonly Mediator _instance = new Mediator();
+
+		public static Mediator GetMediatorInstance
+        {
+            get
+			{
+                return _instance;
+			}
+        }
+	}
+}
